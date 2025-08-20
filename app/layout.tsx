2d3b0cfac8da34be847bb725220914ca012a0442
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-20 15:22:35 +0530

    bug fixed in hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link 
-          rel="preconnect" 
-          href="https://fonts.gstatic.com" 
-          crossOrigin="anonymous" 
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Space+Grotesk:wght@400;500;600;700;800&display=swap"
@@ -64,7 +64,7 @@ export default function RootLayout({
         <ThemeProvider>
           <CursorBurst />
           <Header />
-          <main className="pt-20 md:pt-24">
+          <main>
             {children}
           </main>
           <Footer />
