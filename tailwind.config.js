3bbc15222bf24bef48347283525e08e6c2f387c7
Shashank Mishra <mishrashashank2106@gmail.com>
2025-12-24 00:23:14 +0530

    next version bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -33,4 +32,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
